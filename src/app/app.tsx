66a5a8490bb2450eee0a7f7c96fbae510f93a74b
Onkar Joshi <onkar.joshi@knorex.com>
2018-09-21 15:19:01 +0530

    some tweaks and transition

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -4,6 +4,7 @@ import MatAppBar from '../custom-components/appbar/appbar';
 import * as m from '../custom-components/materialui/materialui-components';
 import styled from 'styled-components';
 import { withStyles } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
 
 export interface AppConfig { text: string };
 
@@ -12,7 +13,10 @@ export class App extends React.Component<AppConfig, any> {
     constructor(props: AppConfig) {
         super(props);
         this.state = {
-            tabValue: 0
+            tabValue: 0,
+            appBar: {
+                class: 'big'
+            }
         };
     }
 
@@ -25,7 +29,12 @@ export class App extends React.Component<AppConfig, any> {
 
     onChangeTab = (event: any, value: any) => {
         this.setState((state: any) => {
-            return { tabValue: value};
+            return {
+                tabValue: value,
+                appBar: {
+                    class: (value > 0) ? 'small' : 'big'
+                }
+            };
         });
     };
 
@@ -33,11 +42,11 @@ export class App extends React.Component<AppConfig, any> {
         const { text } = this.props;
         const { tabValue } = this.state;
         return (
-            <div>
-                <MatAppBar position="static" color="primary" height={230} className>
+            <AppContainer>
+                <MatAppBar position="static" color="primary" className={['app-bar', this.state.appBar.class].join(' ')}>
                     <MatText className="app-bar-title" color="inherit" variant="display3" align="left" weight="lighter" opacity={0.8}>{text}</MatText>
                     <m.Tabs
-                        classes={{root: 'tab-bar'}}
+                        classes={{root: 'tab-bar', flexContainer: 'tabs-flex-container'}}
                         value={tabValue}
                         onChange={this.onChangeTab}
                         indicatorColor="secondary"
@@ -46,7 +55,7 @@ export class App extends React.Component<AppConfig, any> {
                         scrollButtons="off"
                     >
                         {this.tabItems.map((item, index) => {
-                            return <m.Tab classes={{selected: 'tab-selected', root: 'tab-button'}} key={index} label={item.label}></m.Tab>
+                            return <m.Tab classes={{selected: 'tab-selected', root: 'tab-buttons'}} key={index} label={item.label}></m.Tab>
                         })}
                     </m.Tabs>
                 </MatAppBar>
@@ -55,7 +64,23 @@ export class App extends React.Component<AppConfig, any> {
                         return <div className="content-container" key={index}>{item.content}</div>
                     }
                 })}
-            </div>
+            </AppContainer>
         );
     }
 };
+
+export class AppContainer extends React.Component<any, any> {
+
+    constructor(props: any) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div className="app-container">
+                <CssBaseline />
+                {this.props.children}
+            </div>
+        )
+    }
+}
