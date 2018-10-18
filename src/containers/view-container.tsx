97692c363e7b541764c00b064d0e37e72ddaf448
Onkar Joshi <onkar.joshi@knorex.com>
2018-10-18 08:51:01 +0530

    refactor code structure

--- src/containers/view-container.tsx
+++ src/containers/view-container.tsx
@@ -1,5 +1,5 @@
 import * as React from "react";
-import MatText from '../custom-components/text/text';
+import MatText from '../components/text/text';
 
 export class ViewContainer extends React.Component<any, any> {
     constructor(props: any) {
--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { Provider } from 'react-redux';
 import { store } from './store/index';
-import theme from './custom-components/materialui/materialui_module';
+import theme from './components/materialui/materialui_module';
 import { App } from "./app/app";
 import { MuiThemeProvider } from '@material-ui/core/styles';
 import { BrowserRouter } from 'react-router-dom';
